1f446619f2eb981c99b4589f7ccb3df24743fc28
NITESH AGRAHARI <76960456+niteshagrahari@users.noreply.github.com>
2021-11-14 13:38:28 +0530

    maintain two client

--- Network Programming/clientprogram.py
+++ Network Programming/clientprogram.py
@@ -18,7 +18,7 @@ class ReadMessageThread(threading.Thread):
 
 s = socket.socket()
 
-port = 779
+port = 7789
 
 c=s.connect(('127.0.0.1', port))
 while True:
--- Network Programming/serverprogram.py
+++ Network Programming/serverprogram.py
@@ -18,11 +18,27 @@ class ReadMessageThread(threading.Thread):
             print("by Client: ", message)
 
 
+class SendMessageThread(threading.Thread):
+    def __init__(self,c, thread_name, thread_ID):
+        threading.Thread.__init__(self)
+        self.thread_name = thread_name
+        self.thread_ID = thread_ID
+        self.c=c
+
+        # helper function to execute the threads
+
+    def run(self):
+        while True:
+            print('Got connection from', addr, "\n Give your message\n")
+            message = input()
+            c.send(message.encode())
+
+
 
 
 s = socket.socket()
 print("Socket successfully created")
-port = 779
+port = 7789
 s.bind(('', port))
 print("socket binded to %s" % (port))
 s.listen(5)  # 5 incoming connections will be queued up
@@ -31,15 +47,13 @@ while True:
     c, addr = s.accept()
     thread1 = ReadMessageThread(c,"One", 100)
 
-    thread1.start()
-    while True:
-        print('Got connection from', addr, "\n Give your message\n")
-        message = input()
 
-        c.send(message.encode())
+    thread2=SendMessageThread(c,"two",101)
+    thread1.start()
+    thread2.start()
 
     # Close the connection with the client
-    c.close()
+    #c.close()
 
     # Breaking once connection closed
-    break
+    #break
